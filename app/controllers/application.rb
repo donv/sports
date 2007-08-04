0ee04b1284dc97c4961d361991e012af8e2e32b3
Uwe Kubosch <uwe@kubosch.no>
2007-08-04 14:13:45 +0000

    * Added graph

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,14 +6,27 @@ class ApplicationController < ActionController::Base
   include AuthenticatedSystem
   include Localization
   include WurflUtils
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::AssetTagHelper
 
   session :session_key => '_sports_session_id'
   layout 'mwrt002'
   before_filter :login_from_cookie
+  before_filter :populate_layout
   
   def initialize *args
     super
     @handsets, @fallbacks = load_wurfl_pstore('lib/wurfl/wurfl.pstore')
+  end
+
+  def populate_layout
+    @sidebars = [
+      {
+        :title => 'Tours',
+        :content => image_tag(url_for(:controller => 'tours', :action => :graph_small, :id => 1, :format => 'png')),
+        :options => {:controller => 'tours', :action => :graph, :id => 1, :format => 'png'}
+      }
+    ]
   end
   
 end
--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,3 +1,42 @@
+require 'gruff'
+
 class ToursController < ApplicationController
   active_scaffold :tour
+  
+  def graph_small
+    graph 160
+  end
+  
+  def graph(size = 640)
+    g = Gruff::Line.new(size)
+    g.theme_37signals
+    g.title = l(:chart)
+    g.font = '/usr/share/fonts/bitstream-vera/Vera.ttf'
+    g.legend_font_size = 14
+    g.hide_dots = true
+    #g.colors = %w{blue orange green grey grey lightblue #d7a790}
+    
+    tours = Tour.find(:all, :order => :started_at)
+    
+    g.data(l(:time), tours.map {|t| t.total_time.min + t.total_time.sec / 60})
+    g.data(l(:distance), tours.map {|t| t.distance})
+    
+    g.minimum_value = 0
+    
+    labels = {}
+    tours.each_with_index do |t, i|
+      labels[i] = t.started_at.strftime('%y-%m-%d')
+    end
+    g.labels = labels
+    
+    # g.draw_vertical_legend
+    
+    g.maximum_value = g.maximum_value.to_i
+    
+    send_data(g.to_blob,
+              :disposition => 'inline', 
+    :type => 'image/png', 
+    :filename => "tours_chart.png")
+  end
+  
 end
