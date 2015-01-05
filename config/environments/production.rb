3720d5302f769d791fb73d34dad26d4e672eecbb
Uwe Kubosch <uwe@kubosch.no>
2015-01-05 10:57:45 +0000

    * Run without apache.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,5 +22,5 @@ Rails.application.configure do
   config.log_level = :info
   # config.log_tags = [ :subdomain, :uuid ]
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 end
