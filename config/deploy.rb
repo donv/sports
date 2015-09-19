82cce4df111ef1a5e2ed8c904e4fcaa6406d1bf3
Uwe Kubosch <uwe@kubosch.no>
2015-09-19 06:57:24 +0000

    Switch from mri to jruby since mri wont start

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,7 @@
 set :user, 'capistrano'
 set :default_environment, {'JRUBY_OPTS' => '--dev'}
-# set :migrate_env, 'JRUBY_OPTS="--dev -J-Xmx3G"'
-# default_run_options[:pty] = true
 set :rvm_ruby_version, File.read(File.expand_path('../.ruby-version', File.dirname(__FILE__))).strip
+set :pty, true
 
-# after 'deploy:updating', 'deploy:cleanup'
-
+after 'deploy:updating', 'deploy:cleanup'
+after 'deploy:publishing', 'deploy:restart'
