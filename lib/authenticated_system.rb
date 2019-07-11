a8a1d2edca95a0ae4b42b131a6c43e392f3a8f37
Uwe Kubosch <uwe@datek.no>
2019-07-11 09:28:46 +0200

    Rubocop

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -20,7 +20,7 @@ module AuthenticatedSystem
 
   # Store the given user in the session.
   def current_user=(new_user)
-    session[:user] = (new_user.nil? || new_user.is_a?(Symbol)) ? nil : new_user.id
+    session[:user] = new_user.nil? || new_user.is_a?(Symbol) ? nil : new_user.id
     @current_user = new_user
   end
 
@@ -103,7 +103,7 @@ module AuthenticatedSystem
     return unless cookies[:auth_token] && !logged_in?
 
     user = User.find_by_remember_token(cookies[:auth_token])
-    return unless user && user.remember_token?
+    return unless user&.remember_token?
 
     user.remember_me
     self.current_user = user
